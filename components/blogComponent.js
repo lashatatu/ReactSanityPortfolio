ae5f2633c4a21ea16686f6dd6b10b2f9f7b09539
lasha tatulashvili <lashatatu@gmail.com>
2022-02-18 17:03:39 +0400

    reorganised files, started adding testimonials on sanity side

--- components/blogComponent.js
+++ components/blogComponent.js
@@ -6,10 +6,10 @@ const BlogComponent = ({posts}) => {
 	return (
 		 <div>
 			 <h1>Welcome to a blog!</h1>
-			 {posts.length > 0 && posts.map(({_id, title = "", slug = "", publishedAt = ""}) => slug && (
+			 {posts.length > 0 && posts.map(({_id, title = "", slug = "", publishedAt = "", imgUrl}) => slug && (
 					<li className={styles.card} key={_id}>
 						<Link href={"/post/[slug]"} as={`/post/${slug.current}`}>
-							<a>{title}</a>
+							<a>{title}, {imgUrl}</a>
 						</Link>{" "}
 						<p>({new Date(publishedAt).toDateString()})</p>
 
