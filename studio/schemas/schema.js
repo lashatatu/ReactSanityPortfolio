de18320a3510cd160898d74e17bcf26dadfe6808
lasha tatulashvili <lashatatu@gmail.com>
2022-02-19 09:35:06 +0400

    sanity done

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,10 +1,17 @@
 import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
-import testimonials from "./testimonials";
+import awards from "./awards";
+import abouts from "./abouts";
+import brands from "./brands";
+import contact from "./contact";
+import experiences from "./experiences";
+import skills from "./skills";
+import workExperience from "./workExperience";
+import works from "./works";
 
 export default createSchema({
 	name: "default",
 	types: schemaTypes.concat([
-		testimonials,
+		works, awards, brands, abouts, skills, workExperience, experiences, contact,
 	]),
 });
