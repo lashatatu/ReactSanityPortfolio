e21cf29ee81859fb2d292fa678b98bf3207acebf
lasha tatulashvili <lashatatu@gmail.com>
2022-02-19 10:27:54 +0400

    file structure done + removed crappy font

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { About, Awards, Footer, Header, Skills, Work } from "./container";
+import { Navbar } from "./components";
+import './App.scss';
 
 const App = () => {
 	return (
-		 <div>
-			 App
+		 <div className={"app"}>
+			 <Navbar/>
+			 <Header/>
+			 <About/>
+			 <Work/>
+			 <Skills/>
+			 <Awards/>
+			 <Footer/>
 		 </div>
 	);
 };
---
+++ react-frontend/src/container/Awards/Awards.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Awards.scss';
+const Awards = () => {
+	return (
+		 <div>
+			 Awards
+		 </div>
+	);
+};
+
+export default Awards;
