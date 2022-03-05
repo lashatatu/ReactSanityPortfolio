b0ff1feb39e47ed65e8f33d810967ff3fd7b13e0
lasha tatulashvili <lashatatu@gmail.com>
2022-03-05 18:11:23 +0400

    about finished

--- react-frontend/src/container/About/About.jsx
+++ react-frontend/src/container/About/About.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import "./About.scss";
+import {images} from "../../constants";
+import './About.scss';
+
 
 const abouts = [
-	{title: "Front-End", description: "I am a good web developer.", imgUrl: ""},
-	{title: "Back-End", description: "I am a good web developer.", imgUrl: ""},
-	{title: "UI/UX", description: "I am a good web developer.", imgUrl: ""},
+	{title: "Front-End", description: "I am a good web developer.", imgUrl: images.about01},
+	{title: "Back-End", description: "I am a good web developer.", imgUrl: images.about02},
+	{title: "UI/UX", description: "I am a good web developer.", imgUrl: images.about03},
+	{title: "Web Animations", description: "I am a good web developer.", imgUrl: images.about04},
 ];
 
 const About = () => {
 	return (
 		 <>
 			 <h2 className={"head-text"}>
-				 I Know That
-				 <span>Good Design</span>
-				 means
-				 <span>Good Business</span>
+				 I Know That <span>Good Dev </span> <br/>means <span>Good Business</span>
 			 </h2>
 			 <div className={"app__profiles"}>
 				 {abouts.map((about, index) => (
@@ -23,14 +23,12 @@ const About = () => {
 							 whitelInView={{opacity: 1}}
 							 whileHover={{scale: 1.1}}
 							 transition={{duration: 0.5, type: "tween"}}
-							 className={"app_profile-item"}
+							 className={"app__profile-item"}
 							 key={about.title + index}>
 							<img src={about.imgUrl} alt={about.title}/>
-							<h2 className={'bold-text'}>
-
-							</h2>
+							<h2 className={'bold-text'} style={{marginTop:'20px'}}>{about.title}</h2>
+							<p className={'p-text'} style={{marginTop:'10px'}}>{about.description}</p>
 						</motion.div>
-
 				 ))}
 			 </div>
 		 </>);
