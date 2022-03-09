947d91b882fd79dca3b1c40cb5d36ba2b4ab306b
lasha tatulashvili <lashatatu@gmail.com>
2022-03-10 00:29:25 +0400

    skills version done

--- react-frontend/src/container/About/About.jsx
+++ react-frontend/src/container/About/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import {AppWrap} from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import "./About.scss";
 import { urlFor, client } from "../../client";
 
@@ -8,10 +8,9 @@ const About = () => {
 	const [abouts, setAbouts] = useState([]);
 
 	useEffect(() => {
-		const query = '*[_type == "abouts"]';
+		const query = "*[_type == \"abouts\"]";
 
-		client.fetch(query)
-		.then((data) => {
+		client.fetch(query).then((data) => {
 			setAbouts(data);
 		});
 	}, []);
@@ -26,15 +25,15 @@ const About = () => {
 			 <div className="app__profiles">
 				 {abouts.map((about, index) => (
 						<motion.div
-							 whileInView={{ opacity: 1 }}
-							 whileHover={{ scale: 1.1 }}
-							 transition={{ duration: 0.5, type: 'tween' }}
+							 whileInView={{opacity: 1}}
+							 whileHover={{scale: 1.1}}
+							 transition={{duration: 0.5, type: "tween"}}
 							 className="app__profile-item"
 							 key={about.title + index}
 						>
-							<img src={urlFor(about.imgUrl)} alt={about.title} />
-							<h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
-							<p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+							<img src={urlFor(about.imgUrl)} alt={about.title}/>
+							<h2 className="bold-text" style={{marginTop: 20}}>{about.title}</h2>
+							<p className="p-text" style={{marginTop: 10}}>{about.description}</p>
 						</motion.div>
 				 ))}
 			 </div>
@@ -42,4 +41,8 @@ const About = () => {
 	);
 };
 
-export default AppWrap(About,'about');
+export default AppWrap(
+	 MotionWrap(About, "app__about"),
+	 "about",
+	 "app__whitebg",
+);
