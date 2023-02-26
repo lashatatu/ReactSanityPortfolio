fb0e7d1c333e8d2279fd794961a7a342d480c55a
lasha <lashatatu@gmail.com>
2023-02-26 15:18:45 +0400

    added image optimization for better image on Work.jsx file

--- studio/schemas/works.js
+++ studio/schemas/works.js
@@ -1,60 +1,59 @@
 export default {
-  name: 'works',
-  title: 'Works',
-  type: 'document',
+  name: "works",
+  title: "Works",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
     },
 
     {
-      name: 'description',
-      title: 'Description',
-      type: 'string',
+      name: "description",
+      title: "Description",
+      type: "string",
     },
     {
-      name: 'projectLink',
-      title: 'Project Link',
-      type: 'string',
+      name: "projectLink",
+      title: "Project Link",
+      type: "string",
     },
     {
-      name: 'codeLink',
-      title: 'Code Link',
-      type: 'string',
+      name: "codeLink",
+      title: "Code Link",
+      type: "string",
     },
     {
-      name: 'imgUrl',
-      title: 'ImageUrl',
-      type: 'image',
+      name: "imgUrl",
+      title: "ImageUrl",
+      type: "image",
       options: {
         hotspot: true,
       },
     },
     {
-      name: 'tags',
-      title: 'Tags',
-      type: 'array',
+      name: "tags",
+      title: "Tags",
+      type: "array",
       of: [
         {
-          name: 'tag',
-          title: 'Tag',
-          type: 'string',
+          name: "tag",
+          title: "Tag",
+          type: "string",
         },
       ],
     },
     {
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
+      name: "categories",
+      title: "Categories",
+      type: "array",
       of: [
         {
-          type: 'reference',
-          to: {type: 'tags'},
+          type: "reference",
+          to: { type: "tags" },
         },
       ],
     },
-
   ],
 };
