e49fdf34236f79d3f5332f8d3b0ddade93456c11
lasha <lashatatu@gmail.com>
2023-02-26 15:00:50 +0400

    added image optimization for better image on Work.jsx file

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,17 +1,18 @@
-import createSchema from "part:@sanity/base/schema-creator";
-import schemaTypes from "all:part:@sanity/base/schema-type";
-import awards from "./awards";
-import abouts from "./abouts";
-import brands from "./brands";
-import contact from "./contact";
-import experiences from "./experiences";
-import skills from "./skills";
-import workExperience from "./workExperience";
-import works from "./works";
+import createSchema from 'part:@sanity/base/schema-creator';
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+import awards from './awards';
+import abouts from './abouts';
+import brands from './brands';
+import contact from './contact';
+import experiences from './experiences';
+import skills from './skills';
+import workExperience from './workExperience';
+import works from './works';
+import tags from './tags';
 
 export default createSchema({
-	name: "default",
-	types: schemaTypes.concat([
-		works, awards, brands, abouts, skills, workExperience, experiences, contact,
-	]),
+  name: 'default',
+  types: schemaTypes.concat([
+    works, awards, brands, abouts, skills, workExperience, experiences, contact, tags,
+  ]),
 });
---
+++ studio/schemas/tags.js
@@ -0,0 +1,21 @@
+export default {
+  name: "tags",
+  title: "Tags",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "icon",
+      title: "Icon",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+
+  ],
+};
